889368fbf45fd230b93dd05bab01b3fcdb8a471b
MorVered7 <mvered@uw.edu>
2023-02-15 16:09:12 -0800

    Working on Chart 2

--- chart1_example.R
+++ chart1_example.R
@@ -1,6 +1,24 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+library("dplyr")
+library("ggplot2")
 
-library(ggplot2)
-ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+book_checkouts <- read.csv("C:/Users/mvere/OneDrive/Documents/2013-2023-5-Checkouts-SPL.csv", stringsAsFactors = FALSE)
+
+
+physical_values <- book_checkouts %>% 
+  group_by(CheckoutYear) %>% 
+  filter(UsageClass == "Physical") %>% 
+  summarise(avg_physical = mean(Checkouts))
+digital_values <- book_checkouts %>% 
+  group_by(CheckoutYear) %>% 
+  filter(UsageClass == "Digital") %>% 
+  summarise(avg_digital = mean(Checkouts)) 
+book_type <- inner_join(physical_values, digital_values, join_by("CheckoutYear"))
+
+
+ggplot(book_type) +
+  geom_line(mapping = aes(x = CheckoutYear, y = avg_physical, color = "Physical")) +
+  geom_line(mapping = aes(x = CheckoutYear, y = avg_digital, color = "Digital")) +
+  labs(title = "Physical VS Digital Book Checkout Over A Decade", x = "Year", y = "# of Books Checked Out Per Person", color = "Type of Book") + 
+  scale_x_continuous(breaks = seq(2013, 2023, 2))
+  
+  
--- chart2_example.R
+++ chart2_example.R
@@ -1,6 +1,34 @@
-x_values <- seq(1, 3)
-y_values <- seq(1,3)
+library("dplyr")
+library("ggplot2")
 
-library(ggplot2)
+freegal_checkouts <- book_checkouts %>% 
+  group_by(CheckoutYear, CheckoutType) %>% 
+  summarise(avg_freegal = mean(Checkouts)) %>% 
+  filter(CheckoutType == "Freegal")
+
+hoopla_checkouts <- book_checkouts %>% 
+  group_by(CheckoutYear, CheckoutType) %>% 
+  summarise(avg_hoopla = mean(Checkouts)) %>% 
+  filter(CheckoutType == "Hoopla")
+
+horizon_checkouts <- book_checkouts %>% 
+  group_by(CheckoutYear, CheckoutType) %>% 
+  summarise(avg_horizon = mean(Checkouts)) %>% 
+  filter(CheckoutType == "Horizon")
+
+overdrive_checkouts <- book_checkouts %>% 
+  group_by(CheckoutYear, CheckoutType) %>% 
+  summarise(avg_overdrive = mean(Checkouts)) %>% 
+  filter(CheckoutType == "OverDrive")
+
+zinio_checkouts <- book_checkouts %>% 
+  group_by(CheckoutYear,CheckoutType) %>% 
+  summarise(avg_zinio = mean(Checkouts)) %>% 
+  filter(CheckoutType == "Zinio")
+
 ggplot() +
-  geom_line(aes(x=x_values, y = y_values))
+  geom_line(freegal_checkouts, mapping = aes(x = CheckoutYear, y = avg_freegal), color = "Blue") +
+  geom_line(hoopla_checkouts, mapping = aes(x = CheckoutYear, y = avg_hoopla), color = "Red") +
+  geom_line(horizon_checkouts, mapping = aes(x = CheckoutYear, y = avg_horizon), color = "saddlebrown") +
+  geom_line(overdrive_checkouts, mapping = aes(x = CheckoutYear, y = avg_overdrive), color = "brown4") +
+  geom_line(zinio_checkouts, mapping = aes(x = CheckoutYear, y = avg_zinio), color = "hotpink1") 
